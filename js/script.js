690fbb64f824d8ad9ddff354897bdfe263362b85
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2022-03-10 12:52:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strick';
+
+let body = document.querySelector('body');
+let bodyWidth = body.clientWidth;
+if(bodyWidth > 768){
+	let phone = document.querySelector('.phone');
+	phone.innerHTML = '+375 33 375 98 89';
+}
+
+
